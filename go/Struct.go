9bfc669e4428a654a0a6f2f89ff5713edc77422c
AlexandreRocchi <alexandre.rocchi@mailo.com>
2022-05-17 15:10:04 +0200

    FIX problème d'html et css

--- go/Struct.go
+++ go/Struct.go
@@ -13,3 +13,19 @@ type Topic struct {
 	Author string
 	Date   string
 }
+
+type Data_User struct {
+	ID    int64
+	Nom   string
+	email string
+	mdp   string
+	admin string
+}
+
+type Data_Verif struct {
+	Connect_Verif bool
+}
+
+type Data_Topic struct {
+	Nom string
+}
--- go/main.go
+++ go/main.go
@@ -36,14 +36,78 @@ func main() {
 
 	fmt.Println("Connected!")
 
-	css := http.FileServer(http.Dir("../css/"))
-	http.Handle("/css/", http.StripPrefix("/css/", css))
-	images := http.FileServer(http.Dir("../images/"))
-	http.Handle("/images/", http.StripPrefix("/images/", images))
+	index := template.Must(template.ParseFiles("../html/inscrire.html"))
+	http.HandleFunc("/inscrire", func(w http.ResponseWriter, r *http.Request) {
+		UserInput := r.FormValue("UserInput")
+		UserInput2 := r.FormValue("UserInput2")
+		UserInput3 := r.FormValue("UserInput3")
+		NewUser, err := AddData_User(Data_User{
+			ID:    000000,
+			Nom:   UserInput,
+			email: UserInput2,
+			mdp:   UserInput3,
+			admin: "false",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(NewUser)
+		index.Execute(w, NewUser)
+	})
+
+	index2 := template.Must(template.ParseFiles("../html/connecter.html"))
+	http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
+		User := Data_User{
+			Nom: r.FormValue("UserInput"),
+			mdp: r.FormValue("UserInput2"),
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(User.Nom)
+		fmt.Println(User.mdp)
+		Verif := Data_Verif{
+			Connect_Verif: VerifUser(User.Nom, User.mdp),
+		}
+		Verif.Connect_Verif = VerifUser(User.Nom, User.mdp)
+		if Verif.Connect_Verif {
+			http.Redirect(w, r, "/principal", http.StatusSeeOther)
+		}
+		index2.Execute(w, User)
+	})
+
+	index3 := template.Must(template.ParseFiles("../html/principal.html"))
+	http.HandleFunc("/principal", func(w http.ResponseWriter, r *http.Request) {
+		Topics := Data_Topic{
+			Nom: "Topic1",
+		}
+		fmt.Println(Topics)
+		index3.Execute(w, Topics)
+	})
+
+	D1 := template.Must(template.ParseFiles("../html/Souls1.html"))
+	http.HandleFunc("/D1", func(w http.ResponseWriter, r *http.Request) {
+
+		D1.Execute(w, nil)
+
+	})
+
+	D2 := template.Must(template.ParseFiles("../html/Souls2.html"))
+	http.HandleFunc("/D2", func(w http.ResponseWriter, r *http.Request) {
+
+		D2.Execute(w, nil)
+
+	})
+
+	D3 := template.Must(template.ParseFiles("../html/Souls3.html"))
+	http.HandleFunc("/D3", func(w http.ResponseWriter, r *http.Request) {
+
+		D3.Execute(w, nil)
+
+	})
 
 	TemplateMessage := template.Must(template.ParseFiles("../html/TemplateMessage.html"))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/randomtopic", func(w http.ResponseWriter, r *http.Request) {
 
 		tab_messages, err := MessagesPrint()
 		if err != nil {
@@ -64,12 +128,16 @@ func main() {
 				fmt.Println(err)
 				return
 			}
-			http.Redirect(w, r, "/", http.StatusSeeOther)
+			http.Redirect(w, r, "/randomtopic", http.StatusSeeOther)
 		}
 
 		TemplateMessage.Execute(w, message)
 	})
 
+	css := http.FileServer(http.Dir("../css/"))
+	http.Handle("/css/", http.StripPrefix("/css/", css))
+	images := http.FileServer(http.Dir("../images/"))
+	http.Handle("/images/", http.StripPrefix("/images/", images))
 	http.ListenAndServe(":8010", nil)
 
 }
