22bc61e6d9278911660acb358d15481267974a4a
mateo.perrotnasi@ynov.com <mateo.perrotnasi@ynov.com>
2022-05-22 13:44:21 +0200

    ADD new sql file

--- data-access.sql
+++ data-access.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 21 mai 2022 à 11:17
+-- Généré le : dim. 22 mai 2022 à 11:43
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -36,37 +36,19 @@ CREATE TABLE IF NOT EXISTS `data_message` (
   `Topic_ID` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `Topic_ID` (`Topic_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=114 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=135 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `data_message`
 --
 
 INSERT INTO `data_message` (`id`, `content`, `author`, `date`, `Topic_ID`) VALUES
-(111, 'Donc en théorie ça pmarche\r\n', 'Random', '2022-05-20', 124),
-(112, 'dernier petit test', 'kris', '2022-05-20', 124),
-(110, 'pleaaaase', 'Random', '2022-05-20', 124),
-(109, 'zzz', 'Random', '2022-05-20', 124),
-(108, 'rrrrr', 'Random', '2022-05-20', 124),
-(106, 'WtF', 'Random', '2022-05-20', 124),
-(100, 'Fonctionne stp', 'Random', '2022-05-20', 120),
-(107, 'alllerrrr', 'Random', '2022-05-20', 124),
-(104, 'On prie fort', 'Random', '2022-05-20', 124),
-(105, 'SUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU', 'Random', '2022-05-20', 124),
-(113, 'yiiiiiiiiiiiiiiiiiiiiiiheah', 'kris', '2022-05-20', 120);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `data_tags`
---
-
-DROP TABLE IF EXISTS `data_tags`;
-CREATE TABLE IF NOT EXISTS `data_tags` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(128) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+(134, 'Franchement ez', 'Random', '2022-05-22', 7),
+(133, 'wxcvbn,', 'Random', '2022-05-22', 6),
+(132, 'qsdfg', 'Random', '2022-05-22', 5),
+(126, 'aller la', 'Random', '2022-05-21', 2),
+(127, 'fefe', 'kris', '2022-05-21', 3),
+(128, 'Yo KRis', 'kris', '2022-05-21', 3);
 
 -- --------------------------------------------------------
 
@@ -77,11 +59,24 @@ CREATE TABLE IF NOT EXISTS `data_tags` (
 DROP TABLE IF EXISTS `data_topic`;
 CREATE TABLE IF NOT EXISTS `data_topic` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Nom` varchar(255) NOT NULL,
-  `Tag_ID` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `Tag_ID` (`Tag_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Name` varchar(255) NOT NULL,
+  `IsAide` tinyint(1) NOT NULL,
+  `IsBug` tinyint(1) NOT NULL,
+  `IsBoss` tinyint(1) NOT NULL,
+  `IsLore` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `data_topic`
+--
+
+INSERT INTO `data_topic` (`ID`, `Name`, `IsAide`, `IsBug`, `IsBoss`, `IsLore`) VALUES
+(2, 'Bon', 0, 0, 0, 0),
+(3, 'grg', 0, 0, 0, 0),
+(5, 'azerty', 1, 0, 0, 0),
+(6, 'qsdfghj', 1, 0, 1, 0),
+(7, 'O&S  ne sont absolument pas compliqués', 0, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -97,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `data_user` (
   `Password` varchar(255) NOT NULL,
   `Admin` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=218 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=219 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `data_user`
@@ -106,7 +101,8 @@ CREATE TABLE IF NOT EXISTS `data_user` (
 INSERT INTO `data_user` (`ID`, `Name`, `Email`, `Password`, `Admin`) VALUES
 (192, 'Alex', 'alex@gmail.com', '123', 'false'),
 (212, 'Krisfraude', 'krisfraude@gmail.com', 'test1212', 'false'),
-(213, 'Kris', 'Kris@gmail.com', '123', 'false');
+(213, 'Kris', 'Kris@gmail.com', '123', 'false'),
+(218, 'caca', 'caca@gmail.Com', 'caca', 'false');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
